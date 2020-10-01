9fb5a885ccb4e59b1ca1c4dfb489d2abcf8ef06f
Darkness-Scholar <71477885+Darkness-Scholar@users.noreply.github.com>
2020-10-01 14:09:48 +0700

    Update Bai2.py

--- Bai2.py
+++ Bai2.py
@@ -1,1 +1,69 @@
 
+'''
+$2 / TOÁN TỬ TRONG PYTHON
+1. Toán tử số học
+- Phép cộng (+)
+  VD: 3 + 4
+- Phép trừ (-)
+  VD: 5 - 1
+- Phép nhân (*)
+  VD: 3 * 7
+- Phép chia (/)
+  VD: 10 / 2
+- X mũ y (**)
+  VD: 4**2 kết quả là 16
+- Phép chia chuẩn (//): 
+  VD: 5 // 2 kết quả là 2
+- Phép chia chỉ lấy số dư (%)
+  VD: 7 % 2 kết quả là 1 (Vì 7 : 3 = 2 dư 1)
+
+2. Toán tử quan hệ
+- Toán tử quan hệ sẽ trả về True nếu đúng hoặc False nếu sai
+- Lớn hơn (>)
+  VD: 5 > 2 kết quả trả về là True
+- Nhỏ hơn (<)
+  VD: 4 < 6 kết quả trả về là False
+- Lớn hơn hoặc bằng (>=)
+- Nhỏ hơn hoặc bằng (<=)
+- Bằng (==)
+  VD: 5 == 5 trả về True
+- Khác, không bằng (!=) hoặc (<>)
+  VD: 6 <> 9
+
+3. Toán tử gán
+- Sử dụng để gán giá trị cho biến (Bài 3)
+- <Kiến thức này sử dụng ở bài 3 trở đi lên bạn ko phải để tâm trong bài này>
+- Phén gán (=): Gán 1 giá trị cho x
+- Phép cộng và gán (+=)
+  Ngầm hiểu là x = x + y
+- Phép trừ và gán (-=)
+- Phép nhân và gán (*=)
+- Phép chia và gán (/=)
+- Lũy thừa và gán (**=)
+- Phép chia chuẩn và gán (//=)
+- Phép chia lấy dư và gán (%=)
+
+4. Toán tử logic
+- Toán tử và (and)
+  Cho VD sau: (5 > 2) and (6 < 9)
+  Khi 1 trong 2 vế sai, kết quả trả về sẽ là False
+  Toán tử and trả về true khi các vế của nó đều True
+- Toán tử hoặc (or)
+  Khác với and, chỉ cần 1 trong 2 vế là True thì kết quả sẽ là True
+  VD: (5 == 5) or (5 > 9)
+- Phép phủ định (not)
+  Sử dụng để phủ định một phép so sánh
+  (?): not (5 > 3) sẽ trả về gì?
+
+5. Toán tử khai thác
+- Toán tử in (thuộc)
+  Trả về True nếu a thuộc A hoặc ngược lại
+  VD: 1 in [1, 2, 3]
+- Toán tử not in (không thuộc)
+
+6. Toán tử xác thực 
+- Toán tử is (là)
+  Nó đồng nghĩa với toán tử ==
+- Toán tử not is (không là)
+  Đồng nghĩa với !=
+'''
